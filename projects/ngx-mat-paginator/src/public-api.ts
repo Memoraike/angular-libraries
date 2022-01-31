318f55f26f1c431f62fa4a57a4721bca2e85c6f9
Konstantin Kocha <konstantin.kocha@teligent.ru>
2022-01-31 04:49:40 +0300

    Check notes
     - add paginator intl and directive to public api

--- projects/ngx-mat-paginator/src/public-api.ts
+++ projects/ngx-mat-paginator/src/public-api.ts
@@ -4,3 +4,5 @@
 
 export * from './lib/ngx-mat-paginator.component';
 export * from './lib/ngx-mat-paginator.module';
+export * from './lib/ngx-mat-paginator.directive';
+export * from './lib/ngx-mat-paginator-intl';
